bdaffbe1ebac722798c627f254a67ed7a058f596
goodeesjb <goodeesj@gmail.com>
2026-07-31 19:24:53 +0900

    Initial project setup

---
+++ backend/src/test/java/com/devlogai/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlogai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
